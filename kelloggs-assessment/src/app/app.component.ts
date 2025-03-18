7037176168a54016ef7d7a6d8f2a9551de072fb5
Marno-Warp <marno@warpdevelopment.com>
2025-03-18 08:50:41 +0200

    Generated the necessary components and updated main app component

--- kelloggs-assessment/src/app/app.component.ts
+++ kelloggs-assessment/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HowToPlayComponent } from './how-to-play/how-to-play.component';
+import { PrizesComponent } from './prizes/prizes.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HowToPlayComponent, PrizesComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ kelloggs-assessment/src/app/prizes/prizes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prizes',
+  imports: [],
+  templateUrl: './prizes.component.html',
+  styleUrl: './prizes.component.scss'
+})
+export class PrizesComponent {
+
+}
