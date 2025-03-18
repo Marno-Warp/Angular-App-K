f02a096311e22da705b306ad127e887cba082f8a
Marno-Warp <marno@warpdevelopment.com>
2025-03-18 17:08:34 +0200

    Building how-to-play section

--- kelloggs-assessment/src/app/how-to-play/how-to-play.component.ts
+++ kelloggs-assessment/src/app/how-to-play/how-to-play.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-how-to-play',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './how-to-play.component.html',
   styleUrl: './how-to-play.component.scss'
 })
 export class HowToPlayComponent {
+  isOpen = false;
 
+  toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 }
