c3e57ba8dd55c190cb9b52a8a4b176683ef5d2a3
Marno-Warp <marno@warpdevelopment.com>
2025-03-20 14:47:51 +0200

    Implemented animations in footer component

--- kelloggs-assessment/src/app/footer/footer.component.ts
+++ kelloggs-assessment/src/app/footer/footer.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit {
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+  ngAfterViewInit() {
+    const elements = this.el.nativeElement.querySelectorAll('.tiger-image, .cash-prize-play-slogan');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const target = entry.target as HTMLElement;
+          this.renderer.addClass(target, 'on-screen');
+          observer.unobserve(target); 
+        }
+      });
+    }, { threshold: 0.5 }); 
+
+    elements.forEach((element: HTMLElement) => observer.observe(element));
+  }
 }
