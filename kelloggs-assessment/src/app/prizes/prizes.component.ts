6ced8e309845ab9f94377232e96dac11795df7d5
Marno-Warp <marno@warpdevelopment.com>
2025-03-20 13:57:33 +0200

    Implemented animation for prizes component

--- kelloggs-assessment/src/app/prizes/prizes.component.ts
+++ kelloggs-assessment/src/app/prizes/prizes.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-prizes',
-  imports: [],
   templateUrl: './prizes.component.html',
-  styleUrl: './prizes.component.scss'
+  styleUrls: ['./prizes.component.scss']
 })
-export class PrizesComponent {
+export class PrizesComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const boxes = document.querySelectorAll('.colored-box');
 
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+        }
+      });
+    }, { threshold: 1 });
+
+    boxes.forEach(box => observer.observe(box));
+  }
 }
